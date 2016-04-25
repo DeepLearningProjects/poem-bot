d7881113707736ea6f5576f12b366e66bad73108
hunkim <hunkim@gmail.com>
2016-04-25 16:59:14 +0800

    Added

--- main.py
+++ main.py
@@ -12,18 +12,13 @@ from utils import TextLoader
 from model import Model
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--save_dir', type=str, default='save',
-                   help='model directory to store checkpointed models')
-parser.add_argument('-n', type=int, default=500,
-                   help='number of characters to sample')
-parser.add_argument('--prime', type=str, default=' ',
-                   help='prime text')
-args = parser.parse_args()
+save_dir = 'save'
+num_sample = 500
+prime =  ' '
 
-with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
+with open(os.path.join(save_dir, 'config.pkl'), 'rb') as f:
     saved_args = cPickle.load(f)
-with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'rb') as f:
+with open(os.path.join(save_dir, 'chars_vocab.pkl'), 'rb') as f:
     chars, vocab = cPickle.load(f)
 model = Model(saved_args, True)
 
@@ -31,15 +26,15 @@ s = tf.Session()
 
 s.run(tf.initialize_all_variables())
 saver = tf.train.Saver(tf.all_variables())
-ckpt = tf.train.get_checkpoint_state(args.save_dir)
+ckpt = tf.train.get_checkpoint_state(save_dir)
 if ckpt and ckpt.model_checkpoint_path:
     saver.restore(s, ckpt.model_checkpoint_path)
-print (model.sample(s, chars, vocab, args.n, "the"))
+print (model.sample(s, chars, vocab, num_sample, prime))
 
 def sample(num, prime):
     return model.sample(s, chars, vocab, num, prime)
 
-print (sample(200, "the"))
+#print (sample(200, "the"))
 
 # webapp
 from flask import Flask, jsonify, render_template, request
